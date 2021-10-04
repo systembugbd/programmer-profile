af2d9ed6c84cd31839260412c52d821e0a72a99c
ShahebAli <systembugbd@gmail.com>
2021-10-04 07:52:21 +0600

    next.config updated

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 module.exports = {
-     images:{
-          loader: 'imgix',
-          path: 'https://mir-s3-cdn-cf.behance.net'
-     },
-}
+  images: {
+    loader: "imgix",
+    path: "https://mir-s3-cdn-cf.behance.net",
+  },
+  assetPrefix: "/portfolio/",
+  env: {
+    BACKEND_URL: "/portfolio",
+  },
+};
