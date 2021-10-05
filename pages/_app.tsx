01a659c24a7ad8fef76bc1fa8806c8f386b6e9d0
ShahebAli <systembugbd@gmail.com>
2021-10-05 18:32:42 +0600

    Shadow updated

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,7 +18,7 @@ function MyApp({ Component, pageProps }) {
         <div className="col-span-12 p-4 overflow-hidden text-center bg-gray-100 dark:text-white dark:bg-gray-800 md:col-span-5 xl:my-11 xl:px-5 lg:px-2 lg:col-span-4 xl:col-span-3 md:rounded-3xl lg:rounded-3xl shadow-custom-light dark:shadow-custom-dark ">
           <Sidebar />
         </div>
-        <div className="col-span-12 overflow-hidden bg-gray-100 dark:text-white dark:bg-gray-800 md:col-span-7 xl:my-11 lg:col-span-8 xl:col-span-9 md:rounded-3xl lg:rounded-3xl">
+        <div className="col-span-12 overflow-hidden bg-gray-100 dark:text-white dark:bg-gray-800 md:col-span-7 xl:my-11 lg:col-span-8 xl:col-span-9 md:rounded-3xl lg:rounded-3xl shadow-custom-light dark:shadow-custom-dark">
           <Navbar />
           <Component {...pageProps} />
         </div>
